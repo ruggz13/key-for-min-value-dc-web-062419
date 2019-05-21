ab960779359f67c89789bbee301c5bf4c01d2d17
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-21 18:25:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
 
-  name_hash.each do |k, v|
+  hash.each do |k, v|
     if loweset_value == nil || v < lowest_value
       lowest_value = v
       lowest_key = k
